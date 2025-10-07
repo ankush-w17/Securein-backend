82b6103970c1c39db3e11cce1cc7f72d0f63d2c0
Ankush <ankushwadhwani222@gmail.com>
2025-10-07 12:05:44 +0530

    added app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-there was a minor problem, 
+const express = require('express');
+const cors = require('cors');
+const recipeRoutes = require('./routes/recipeRoutes');
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Recipe API is running!'));
+app.use('/api/recipes', recipeRoutes);
+
+module.exports = app;
