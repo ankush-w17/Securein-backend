1dc4071aa8890d78253741c44732422bbbd776de
Ankush <ankushwadhwani222@gmail.com>
2025-10-07 12:01:58 +0530

    added routers

---
+++ src/routes/recipeRoutes.js
@@ -0,0 +1,9 @@
+
+const express = require('express');
+const router = express.Router();
+const recipeController = require('../controllers/recipeController');
+
+router.get('/', recipeController.getAllRecipes);
+router.get('/search', recipeController.searchRecipes);
+
+module.exports = router;
